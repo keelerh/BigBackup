9083b495ab40df8e82696f4fc577e2e37babb06d
kerhardt <kerhardt@mit.edu>
2016-09-25 05:14:59 -0400

    Merge pgp additions

--- compress_dir.py
+++ compress_dir.py
@@ -2,19 +2,18 @@ from PIL import Image
 import qrcode
 import tarfile
 import subprocess
-import numpy
 import gnupg
 
 MAX_DATA_PER_QR = 2700
 QR_CODES_PER_FRAME = 60
 YOUTUBE_VIDEO_DIMENSIONS = (1920, 1080)
-FFMPEG_BIN = "ffmpeg"
 
 
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, source_dir)
-        
+
+
 def encrypt_tar(output, source, public_key):
     gpg = gnupg.GPG(public_key)
     with open('storage.tar', 'rb') as f:
@@ -22,7 +21,6 @@ def encrypt_tar(output, source, public_key):
         f, recipients=['User'],
         always_trust='true',
         output='storage.tar.gpg')
-    
 
 
 def qr_encode(zipped_dir):
@@ -55,6 +53,9 @@ def qr_encode(zipped_dir):
         qr.make(fit=True)
         img_file = qr.make_image()
         qr_codes.append(img_file)
+    max_len = len(str(len(qr_codes)))
+    for frame, code in enumerate(qr_codes):
+        code.save("frame-" + str(frame).zfill(max_len) + ".png")
     return qr_codes
 
 
@@ -79,21 +80,26 @@ def stitch_images(image_files):
 
 
 def convert_to_mp4(image_files, output_filename):
-    command = [FFMPEG_BIN, '-y',  # (optional) overwrite output file if it exists
-                '-f', 'rawvideo',
-                '-vcodec', 'rawvideo',
-                '-s', '177x177',  # size of one frame
-                '-pix_fmt', 'rgb24',
-                '-r', '24',  # frames per second
-                '-i', '-',  # The imput comes from a pipe
-                '-an',  # Tells FFMPEG not to expect any audio
-                '-vcodec', 'mpeg',
-                output_filename]
-    pipe = subprocess.Popen(command, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    for image in image_files:
-        pipe.stdin.write(image)
-    pipe.stdin.close()
-    pipe.stderr.close()
+    #  command = [FFMPEG_BIN, '-y',  # (optional) overwrite output file if it exists
+    #              '-f', 'rawvideo',
+    #              '-vcodec', 'rawvideo',
+    #              '-s', '177x177',  # size of one frame
+    #              '-pix_fmt', 'rgb24',
+    #              '-r', '24',  # frames per second
+    #              '-i', '-',  # The imput comes from a pipe
+    #              '-an',  # Tells FFMPEG not to expect any audio
+    #              '-vcodec', 'mpeg',
+    #              output_filename]
+    #  pipe = subprocess.Popen(command, stdin=subprocess.PIPE,
+    #          stderr=subprocess.PIPE, shell=True)
+    #  for image in image_files:
+    #      pipe.stdin.write(image.tobytes())
+    #  pipe.stdin.close()
+    #  pipe.stderr.close()
+    subprocess.call(['ffmpeg', '-r', '60', '-s', '177x177',
+        '-i', 'frame-%d.png', 'youtube_test.mp4'],
+        shell=True)
+
 
 if __name__ == '__main__':
     make_tarfile('compressed_test', 'test')
