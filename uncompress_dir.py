13cf578313611f18e05ffee2980d6fb7ee76b759
Artur Tarasenko <arttarawork@gmail.com>
2016-09-25 00:20:15 -0400

    Added video downloading capability to uncompress_dir.py

--- uncompress_dir.py
+++ uncompress_dir.py
@@ -1,1 +1,6 @@
+import os
 
+os.system("sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl")
+os.system("sudo chmod a+rx /usr/local/bin/youtube-dl")
+link = open("link.txt", "w").read()
+os.system("youtube-dl -o '%(timestamp)s' " + link)
