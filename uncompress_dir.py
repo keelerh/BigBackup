e03b8c54cdf9c3263854416a7b16db96e7cdbd9b
Artur Tarasenko <arttarawork@gmail.com>
2016-09-25 04:19:08 -0400

    Processing of each frame is done.

--- uncompress_dir.py
+++ uncompress_dir.py
@@ -1,12 +1,23 @@
-from PIL import Image
-import Image
+"""
+
+Please run the following commands (or your OS's variant of these commands).
+
+You need these things installed.
+
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+sudo apt-get install python-wand
+
+sudo apt-get install ffmpeg
+
+sudo apt-get install python-qrtools
+
+"""
+
+from qrtools import QR
 import os
 import glob
-
-# Installing necessary bullshit. Please use Linux to make it good.
-os.system("sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl")
-os.system("sudo apt-get install ffmpeg")
-os.system("sudo chmod a+rx /usr/local/bin/youtube-dl")
 
 # Downloads the video
 link = open("link.txt", "w").read()
@@ -15,8 +26,23 @@ os.system("youtube-dl -o '%(id)s' https://www.youtube.com/watch?v=" + link)
 # Splits the .mp4 video into many .png frames
 os.system("ffmpeg -i "link".mp4 -r 60 -s 1920x1080 -f image2 frame-%07d.png")
 
-# Splits each frame into its 60 QR codes and processes them.
+# The .tar where things are stored
+storage = open("storage.tar", "w+")
+
+# Processes each frame and appends the data to storage.tar
 lastFrameNumber = max(glob.iglob('*.[Mm][Pp]4'), key=os.path.getctime)
 lastFrameNumber = lastFrameNumber[6:13]
-for x in (0 to lastFrameNumber):
+for frame in range(0 to (lastFrameNumber + 1)):
+	myQR = QR(filename="frame-"+frame.zfill(7)+".png")
+	if myQR.decode():
+		with open("storage.tar", "a") as storage:
+			storage.write(myQR.data)
+		#print myQR.data
+	os.remove("frame-"+frame.zfill(7)+".png")
 
+
+
+	
+
+
+	
