5b56901d332b48c37a703a9fc067fc920f0cdc6d
kerhardt <kerhardt@mit.edu>
2016-09-24 20:44:17 -0400

    Resolved some conflict

--- compress_dir.py
+++ compress_dir.py
@@ -1,23 +1,21 @@
 import Image
+import libarchive.public
+import libarchive.constants
 from os import listdir
 from os.path import isfile, join
 import qrcode
-
-import libarchive.public
-import libarchive.constants
 
 
 MAX_DATA_PER_QR = 2956
 QR_CODES_PER_FRAME = 60
 YOUTUBE_VIDEO_DIMENSIONS = (1920, 1080)
 
-def 7z:
+def compress_7z(directory):
     for entry in libarchive.public.create_file(
                 'qr.7z',
                 libarchive.constants.ARCHIVE_FORMAT_7ZIP,
                 ['/etc/qr4compress']):
     print(entry)
-
 
 def qr_encode(zipped_dir):
     qr_codes = []
@@ -48,11 +46,15 @@ def qr_encode(zipped_dir):
 def stitch_images(image_files):  #TODO
     canvas = Image.new('RGB', YOUTUBE_VIDEO_DIMENSIONS)
     image_count = 0
+    frames = []
     for i in xrange(0,1100,100):
         for j in xrange(0,700,100):
             im = image_files[i]
             im.thumbnail(100, 100)
             canvas.paste(im, (i, j))
+            image_count += 1
+            if image_count >= len(image_files):
+                break
 
 
 def convert_to_video(image_files):  #TODO
@@ -60,5 +62,7 @@ def convert_to_video(image_files):  #TODO
 
 if __name__ == '__main__':
     zipped_dir =  # TODO
+    compressed_images = qr_encode(zipped_dir)
     frames = stitch_images(compressed_images)
     video_file = convert_to_video(frames)
+    # upload to youtube
