cb5b6827538a827672a37b389e1180c79210368d
kerhardt <kerhardt@mit.edu>
2016-09-25 02:28:54 -0400

    Switch to gzip compression

--- compress_dir.py
+++ compress_dir.py
@@ -1,28 +1,24 @@
-import Image
-import libarchive.public
-import libarchive.constants
-from os import listdir
-from os.path import isfile, join
+from PIL import Image
 import qrcode
-import libarchive.public
-import libarchive.constants
-import os
+import tarfile
+import gzip
 
-
-MAX_DATA_PER_QR = 2956
+MAX_DATA_PER_QR = 2900
 QR_CODES_PER_FRAME = 60
 YOUTUBE_VIDEO_DIMENSIONS = (1920, 1080)
+BLOCKSIZE = 1 << 16  # 64kB
 
 
-def zip_dir(directory):
-    filenames = []
-    for root, dirs, files in os.walk(directory):
-        filenames.extend([os.path.join(root, f) for f in files])
-    zipfile = libarchive.public.create_file(
-        'create.7z',
-        libarchive.constants.ARCHIVE_FORMAT_7ZIP,
-        filenames)
-    return zipfile
+def gzipfile(iname, oname, level):
+    with open(iname, 'rb') as f_in:
+        f_out = gzip.open(oname, 'wb', level)
+        while True:
+            block = f_in.read(BLOCKSIZE)
+            if block == '':
+                break
+            f_out.write(block)
+        f_out.close()
+    return
 
 
 def qr_encode(zipped_dir):
@@ -54,29 +50,38 @@ def qr_encode(zipped_dir):
         qr.make(fit=True)
         img_file = qr.make_image()
         qr_codes.append(img_file)
+    for i, qr in enumerate(qr_codes):
+        qr.save("qr" + i + ".png")
     return qr_codes
 
 
-def stitch_images(image_files):  #TODO
+def stitch_images(image_files):
     canvas = Image.new('RGB', YOUTUBE_VIDEO_DIMENSIONS)
     image_count = 0
     frames = []
-    for i in xrange(0,1100,100):
-        for j in xrange(0,700,100):
-            im = image_files[i]
-            im.thumbnail(100, 100)
-            canvas.paste(im, (i, j))
-            image_count += 1
-            if image_count >= len(image_files):
-                break
+    while image_count < len(image_files):
+        for i in xrange(0, 1100, 100):
+            for j in xrange(0, 700, 100):
+                im = image_files[i]
+                im.thumbnail(100, 100)
+                canvas.paste(im, (i, j))
+                image_count += 1
+                if image_count >= len(image_files):
+                    break
+        frames.append(canvas)
+        canvas = Image.new('RGB', YOUTUBE_VIDEO_DIMENSIONS)
+        for i, qr in enumerate(frames):
+            qr.save("frame" + i + ".png")
+    return frames
 
 
-def convert_to_video(image_files):  #TODO
+#def convert_to_video(image_files):  #TODO
 
 
 if __name__ == '__main__':
-    zipped_dir =  # TODO
-    compressed_images = qr_encode(zipped_dir)
+    gzipfile('6.172.zip', 'gzip_is_cooler', 9)
+    compressed_images = qr_encode('6.172.zip')
     frames = stitch_images(compressed_images)
-    video_file = convert_to_video(frames)
+    print frames
+#    video_file = convert_to_video(frames)
     # upload to youtube
