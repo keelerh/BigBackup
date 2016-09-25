1c15a9c78c7f696a88e84ed2004b8a77436dce83
Artur Tarasenko <arttarawork@gmail.com>
2016-09-25 07:18:27 -0400

    Small changes to uncompress and the readme.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # BigBackup
+
+Hello! 
+
+This is a cool thing made by Keeley Erhardt, Payam Ghobadpour, and Artur Tarasenko at HackGT 2016.
+
+It lets you put a lot of stuff on Youtube if you need space sometimes.
+
+You need the following packages, though...
+
+- python-qrcode
+- python-qrtools
+- ffmpeg
+- youtube-dl
+- tarfile
--- uncompress_dir.py
+++ uncompress_dir.py
@@ -7,8 +7,6 @@ You need these things installed.
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 
-sudo apt-get install python-wand
-
 sudo apt-get install ffmpeg
 
 sudo apt-get install python-qrtools
@@ -18,16 +16,19 @@ sudo apt-get install python-qrtools
 from qrtools import QR
 import os
 import glob
+import gnupg
 
 # Downloads the video
 link = open("link.txt", "w").read()
 os.system("youtube-dl -o '%(id)s' https://www.youtube.com/watch?v=" + link)
 
 # Splits the .mp4 video into many .png frames
-os.system("ffmpeg -i "link".mp4 -r 60 -s 1920x1080 -f image2 frame-%07d.png")
+os.system("ffmpeg -i " + link + ".mp4 -r 60 -s 1920x1080 -f image2 frame-%07d.png")
 
 # The .tar where things are stored
-storage = open("storage.tar", "w+")
+storage = open("storage.tar.gpg", "w+")
+
+def decryptTar()
 
 # Processes each frame and appends the data to storage.tar
 lastFrameNumber = max(glob.iglob('*.[Mm][Pp]4'), key=os.path.getctime)
@@ -35,9 +36,8 @@ lastFrameNumber = lastFrameNumber[6:13]
 for frame in range(1 to (lastFrameNumber + 1)):
 	myQR = QR(filename="frame-"+frame.zfill(7)+".png")
 	if myQR.decode():
-		with open("storage.tar", "a") as storage:
+		with open("storage.tar.gpg", "a") as storage:
 			storage.write(myQR.data)
-		#print myQR.data
 	os.remove("frame-"+frame.zfill(7)+".png")
 
 
